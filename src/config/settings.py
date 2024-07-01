beca1754255f03204d95d6d246415a43c5382ef1
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-02 00:46:50 +0900

    task: 주별 업무 API 완료 (유저용)

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "users",
     "departments",
+    "todos",
     "core",
     "rest_framework",
 ]
--- src/core/permissions.py
+++ src/core/permissions.py
@@ -4,16 +4,23 @@ from rest_framework import permissions
 
 class IsHRAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in ["HEAD", "OPTIONS"]:
             return True
         return request.user.groups.filter(name=settings.HR_ADMIN_GROUP_NAME).exists()
 
 
 class IsHRAdminOrSelf(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in ["HEAD", "OPTIONS"]:
             return True
         return (
             obj == request.user
             or request.user.groups.filter(name=settings.HR_ADMIN_GROUP_NAME).exists()
         )
+
+
+class IsTaskAssignee(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ["HEAD", "OPTIONS"]:
+            return True
+        return obj.assignee == request.user
