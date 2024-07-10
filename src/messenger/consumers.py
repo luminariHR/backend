9f2e0218539566e128641f1fca8ab0d404eea990
sequaa <asl0619@naver.com>
2024-07-10 10:50:48 +0900

    한글 인코딩 수정, username 수정, 소켓 연결 url 수정

--- src/messenger/consumers.py
+++ src/messenger/consumers.py
@@ -7,8 +7,8 @@ from asgiref.sync import sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
+        self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        self.room_group_name = f"chat_{self.room_id}"
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
@@ -23,7 +23,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = data["message"]
         employee = self.scope["user"]
         sender = await sync_to_async(Employee.objects.get)(email=employee)
-        chat_room = await sync_to_async(ChatRoom.objects.get)(name=self.room_name)
+        chat_room = await sync_to_async(ChatRoom.objects.get)(id=self.room_id)
 
         await sync_to_async(Message.objects.create)(
             chat_room=chat_room, sender=sender, content=message
@@ -31,13 +31,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.channel_layer.group_send(
             self.room_group_name,
-            {"type": "chat_message", "message": message, "username": Employee.username},
+            {"type": "chat_message", "message": message, "sender_id": sender.email},
         )
 
     async def chat_message(self, event):
         message = event["message"]
-        username = event["username"]
+        sender_id = event["sender_id"]
 
         await self.send(
-            text_data=json.dumps({"message": message, "username": username})
+            text_data=json.dumps(
+                {"message": message, "sender_id": sender_id},
+                ensure_ascii=False,
+            )
         )
