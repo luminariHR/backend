c389501ddb57239cc70c0085804c4ed191c7c4d2
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-13 08:06:22 +0900

    task: 내 프로필 조회, JWT 하루로 변경

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet
+from .views import EmployeeViewSet, MyProfileView
 
 router = DefaultRouter()
 router.register(r"accounts", EmployeeViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("me/", MyProfileView.as_view()),
 ]
