fe830762d9204ded88cb1fbb5b377118ad32c768
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-16 00:21:22 +0900

    task: 전자결재 개별 조회

--- src/approval/urls.py
+++ src/approval/urls.py
@@ -18,7 +18,7 @@ from .views import (
 urlpatterns = [
     # path("", include(router.urls)),
     path("documents/", AgendaReviewRequestCreateView.as_view()),
-    path("documents/<int:agenda_id>/", AgendaReviewView.as_view()),
+    path("documents/<str:agenda_id>/", AgendaReviewView.as_view()),
     path("documents/sent/", SentReviewRequestView.as_view()),
     path("documents/received/", ReceivedReviewRequestView.as_view()),
     path("documents/referenced/", ReferencedReviewRequestView.as_view()),
