421c9e525de4874792d5ebff1dd9ca9334b64e38
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-21 14:27:26 +0900

    task: 인사 발령 노티 컨텍스트 추가

--- src/appointments/signals.py
+++ src/appointments/signals.py
@@ -23,5 +23,13 @@ def apply_appointment(sender, instance, **kwargs):
                 employee.department.head = None
             employee.department.save()
             # 인사 발령 노티
-            message = f"{employee.name} 님, {instance.effective_date} 부로 팀/직책이 변경 되었습니다. 확인해주세요."
-            send_notification(employee.id, message, "appointment_created")
+            message = f"{instance.effective_date} 부로 팀/직책이 변경 되었습니다!"
+
+            context = {
+                "from": {
+                    "name": employee.name,
+                    "profile_image": employee.profile_image.url,
+                },
+                "path": "/myprofile",
+            }
+            send_notification(employee.id, message, "appointment_created", context)
