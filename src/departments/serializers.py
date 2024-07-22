c0abf8c7ea1301d91414d1d1b28ff53a45c608f5
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-22 13:27:56 +0900

    task: added profile image to member serializer

--- src/departments/serializers.py
+++ src/departments/serializers.py
@@ -7,7 +7,7 @@ from users.models import Employee
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ["id", "employee_id", "name", "job_title"]
+        fields = ["id", "employee_id", "name", "job_title", "profile_image"]
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
