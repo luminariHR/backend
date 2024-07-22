2927d01f1d45e814db0c688496a1eaebe96c8f0f
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-22 21:54:28 +0900

    fix: lazy load model

--- src/config/settings.py
+++ src/config/settings.py
@@ -142,8 +142,8 @@ DATABASES = {
 }
 
 if DJANGO_ENV == "local":
-    CELERY_BROKER_URL = "redis://127.0.0.1:6379"
-    CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379"
+    CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+    CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
     LOGIN_URL = "http://localhost:5173/login"
@@ -154,6 +154,7 @@ else:
     STATIC_ROOT = os.path.join("/app/", "static")
     MEDIA_ROOT = os.path.join("/app/", "media")
     LOGIN_URL = "https://ktluminari.netlify.app/login"
+
 
 # 이메일 설정
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
