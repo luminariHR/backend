87d5426b8f3f83d2fb6a69a2b7f90532c1263365
sequaa <asl0619@naver.com>
2024-07-23 10:22:52 +0900

    feat: 노티 모델 수정

--- src/notifications/models.py
+++ src/notifications/models.py
@@ -11,7 +11,9 @@ class Notification(AbstractBaseModel):
     APPOINTMENT_CREATED = "appointment_created"  # 인사발령
     AGENDA_REVIEWED = "agenda_reviewed"
     AGENDA_REQUESTED = "agenda_requested"
-    NEW_MASSEGE = "new_message"
+    NEW_MASSAGE = "new_message"
+    PTO_REQUESTED = "pto_requested"
+    PTO_REVIEWED = "pto_reviewed"
 
     NOTIFICATION_TYPE_CHOICES = (
         (TO_DO_ASSIGNED, "To Do Assigned"),
@@ -19,7 +21,9 @@ class Notification(AbstractBaseModel):
         (APPOINTMENT_CREATED, "Appointment Created"),
         (AGENDA_REVIEWED, "Agenda Reviewed"),
         (AGENDA_REQUESTED, "Agenda Requested"),
-        (NEW_MASSEGE, "New Message"),
+        (NEW_MASSAGE, "New Message"),
+        (PTO_REQUESTED, "PTO Requested"),
+        (PTO_REVIEWED, "PTO Reviewed"),
     )
 
     receiver = models.ForeignKey(Employee, on_delete=models.CASCADE)
