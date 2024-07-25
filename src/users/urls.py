39842bc65ef093720ff590f89a3f48d5dce7b5d0
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-25 22:42:03 +0900

    task: faster user query

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import EmployeeViewSet, MyProfileView, UserInviteView
 
 router = DefaultRouter()
-router.register(r"accounts", EmployeeViewSet)
+router.register(r"accounts", EmployeeViewSet, basename="employee")
 
 urlpatterns = [
     path("", include(router.urls)),
